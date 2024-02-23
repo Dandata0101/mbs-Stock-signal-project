dabafadaae5b14a7b74e57b0a2a024569b188e97
Dan Ramirez <36237964+Dandata0101@users.noreply.github.com>
2024-02-23 19:21:36 +0100

    updated email delivery package

--- scripts/sendemail.py
+++ scripts/sendemail.py
@@ -14,7 +14,6 @@ warnings.filterwarnings("ignore")
 os.system('cls' if os.name == 'nt' else 'clear')
 masterdir = os.getcwd()
 
-
 import configparser
 config = configparser.ConfigParser()
 config.read(masterdir+'/01-config/pw-config.ini')
@@ -22,16 +21,16 @@ tenant_id = config.get('msftparam', 'tenant_id')
 client_id = config.get('msftparam', 'client_id')
 client_secret = config.get('msftparam', 'client_secret')
 
-
-# Set up the variablesc
+# Set up the variables
 tenant_id = tenant_id
 client_id = client_id
 client_secret = client_secret
 resource_url = 'https://graph.microsoft.com'
 api_version = 'v1.0'
-user_email = 'delivery@y-data.co'  # replace with the email address of the user you want to send the email on behalf ofd
-recipient_email = input('Enter the recipient email address: ')
-email_subject = 'Test email'
+user_email = 'mbs-bigdata-delivery@y-data.fr'  # replace with the email address of the user you want to send the email on behalf of
+recipient_emails_input = input('Enter recipient email addresses separated by a comma: ')
+recipient_emails = recipient_emails_input.split(',')  # Split the input string into a list
+email_subject = 'MBS Big data Stock project'
 email_body = 'This is a test email sent using the Microsoft Graph API.'
 
 # Get an access token using client secret
@@ -50,25 +49,21 @@ except KeyError:
     print(f"Failed to retrieve access token: {response.json()['error_description']}")
     exit(1)
 
-# Construct the email message
+# Construct the email message (MIME part, not used with Graph API in this script)
 msg = MIMEMultipart()
 msg['Subject'] = email_subject
 msg['From'] = user_email
-msg['To'] = recipient_email
+# 'To' field in MIME message is not used for multiple recipients in Graph API, shown for completeness
+msg['To'] = ", ".join(recipient_emails) 
 
 # Create and attach the message body
 text = MIMEText(email_body)
 msg.attach(text)
 
 # Attach the file from directory
-attachment_dir = masterdir + '/01-projects/01-segmentation/04-summary/'
+attachment_dir = masterdir + '/03-output/'
 
-# Attach all files in the directory
-for file_path in glob.glob(attachment_dir + '*.xlsx'):
-    with open(file_path, 'rb') as f:
-        attachment = MIMEApplication(f.read(), _subtype='octet-stream')
-        attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(file_path))
-        msg.attach(attachment)
+# Construct email_message for Graph API with multiple recipients
 email_message = {
     "message": {
         "subject": email_subject,
@@ -77,11 +72,7 @@ email_message = {
             "content": email_body
         },
         "toRecipients": [
-            {
-                "emailAddress": {
-                    "address": recipient_email
-                }
-            }
+            {"emailAddress": {"address": email.strip()}} for email in recipient_emails
         ],
         "attachments": [
             {
@@ -105,10 +96,7 @@ response = requests.post(send_email_url, headers=headers, data=json.dumps(email_
 # Check the response
 if response.status_code == 202:
     print('Email sent successfully.')
+    sys.exit(0)  # Exit script after successful email send
 else:
     print('Error sending email.')
     print(response.text)
-
-print("returning to menu")
-print('')
-os.system('python '+masterdir +'/01-projects/01-segmentation/MenuTerminal.py')
