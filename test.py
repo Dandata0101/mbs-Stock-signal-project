5b26bcbed3faed1747b52e9a29e90110d0e70945
Dan Ramirez <36237964+Dandata0101@users.noreply.github.com>
2024-02-27 22:03:31 +0100

    minor update

--- test.py
+++ test.py
@@ -13,11 +13,6 @@ data=create_dataframe(stock)
 
 fx=buysellfx(data)    
 
-newchart=new_plot_stock_signals(df=fx,tickerSymbol=stock)
-chart=plot_stock_signals(df=fx,tickerSymbol=stock)
-export=export_df_to_excel_with_chart(df=fx,tickerSymbol=stock)
-
-
 Body=generate_email_body(tickerSymbol=stock)
 send=send_email(email_body=Body,recipient_emails=email)
 
