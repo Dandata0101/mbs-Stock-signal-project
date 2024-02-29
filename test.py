76df093ae8f8b7470171b00e113f2e5093fee90f
Dan Ramirez <36237964+Dandata0101@users.noreply.github.com>
2024-02-29 10:57:12 +0100

    Removing old files

--- test.py
+++ test.py
@@ -2,10 +2,8 @@
 
 import os
 from scripts.yahoofinance import create_dataframe
-from scripts.buysellfx import buysellfx
 from scripts.ml_buysellfx import predict_trading_signals
 from scripts.ml_chart_export import plot_stock_signals,interactive_plot_stock_signals,Last_record
-
 from scripts.profit_calc import calculate_profit
 from scripts.excel_export import export_df_to_excel_with_chart
 from EmailBody.emailbody import generate_email_body
